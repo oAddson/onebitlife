6cae451575329decd64e8d9833cf6be0309eb08f
Addson Coutinho <addsoncoutinho@hotmail.com>
2022-12-03 10:43:04 -0300

    estilização pagina Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,17 +1,39 @@
 import React from "react";
-import { Image, View, Text, ScrollView } from "react-native";
+import { Image, View, Text, ScrollView, StyleSheet } from "react-native";
 
 export default Start = () => {
-  return (<View>
-    <ScrollView showVerticalScrollIndicator={false}>
-      <View>
-        <Image
-          source={require("../../assets/icons/logo3.png")}
-        />
-        <Text>
-          Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível.  
-        </Text>
-      </View>
-    </ScrollView>
-  </View>)
+  return (
+    <View style={styles.container}>
+      <ScrollView showVerticalScrollIndicator={false}>
+        <View style={{alignItems: "center"}}>
+          <Image
+            style={styles.logo}
+            source={require("../../assets/icons/logo3.png")}
+          />
+          <Text style={styles.description}>
+            Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível.  
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21,21,21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  }
+})
