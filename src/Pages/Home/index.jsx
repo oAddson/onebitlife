62d5a6d22c5584dbb01ca5812b1134ec1c87a8a3
Addson Coutinho <addsoncoutinho@hotmail.com>
2022-12-03 20:15:13 -0300

    criado componente de botão de adicionar habito

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { StyleSheet, View, ScrollView, Text } from "react-native";
 import LifeStatus from "../../Components/Common/LifeStatus";
+import CreateHabit from "../../Components/Home/CreateHabit";
 import StatusBar from "../../Components/Home/StatusBar";
 
 export default Home = () => {
   const navigation = useNavigation();
+  const[mindHabit, setMindHabit] = useState();
+  const[moneyHabit, setMoneyHabit] = useState();
+  const[bodyHabit, setBodyHabit] = useState();
+  const[funHabit, setFunHabit] = useState();
 
   const handleNavExplanation = () => {
     navigation.navigate("AppExplanation")
@@ -18,6 +23,7 @@ export default Home = () => {
           <Text style={styles.dailyChecks}> ❤️ 20 dias - ✔️ 80 checks </Text>
           <LifeStatus />
           <StatusBar />
+          <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
         </View>
         <Text 
           style={styles.explanationText}
