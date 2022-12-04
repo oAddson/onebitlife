952f4c0292790405373375a13dbcbbb686964c3d
Addson Coutinho <addsoncoutinho@hotmail.com>
2022-12-04 14:19:13 -0300

    estrutura das rotas e da homepage

--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -1,28 +1,23 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import Start from "../Pages/Start";
 import AppExplanation from "../Pages/AppExplanation";
 import Home from "../Pages/Home";
-import HabitPage from "../Pages/HabitPage";
 
 const Stack = createNativeStackNavigator();
 
-export default AllPages = () => {
-  return(
+export default HomePage = () => {
+  return (
     <NavigationContainer>
-      <Stack.Navigator 
+      <Stack.Navigator
         screenOptions={{
           headerShown: false,
         }}
       >
-
-        <Stack.Screen name="Start" component={Start}/>
-        <Stack.Screen name="AppExplanation" component={AppExplanation}/>
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="HabitPage" component={HabitPage}/>
-        
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="HabitPage" component={HabitPage} />
+        <Stack.Screen name="AppExplanation" component={AppExplanation} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-};
+  );
+}
+ 
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import AllPages from "./AllPages";
+import HomePage from "./HomePage";
 
 export default Routes = () => {
-  return <AllPages />
+  const [showHome, setShowHome] = useState("false");
+  return <>{ showHome ? <HomePage/> : <AllPages /> }</> 
 }
